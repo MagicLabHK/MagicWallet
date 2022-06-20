a327032a9406730c18d28be8c936d2b673a69fba
eric.cheng <ericcheng201168@gmail.com>
2022-06-21 00:06:18 +0800

    Code cleanup

--- lib/utils/custom_keyboard.dart
+++ lib/utils/custom_keyboard.dart
@@ -2,12 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:keyboard_actions/keyboard_actions.dart';
 
 class CustomKeyboard{
-  static final FocusNode _nodeText1 = FocusNode();
-  static final FocusNode _nodeText2 = FocusNode();
-  static final FocusNode _nodeText3 = FocusNode();
   static final FocusNode numericKeyboard = FocusNode();
-  static final FocusNode _nodeText5 = FocusNode();
-  static final FocusNode _nodeText6 = FocusNode();
 
   static KeyboardActionsConfig buildNumericKeyboardConfig(BuildContext context) {
     return KeyboardActionsConfig(
